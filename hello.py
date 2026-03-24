e6019f053f224fbf2f589ab5800c044f4e379648
Darryl Fonkou <darrylfonkou@gmail.com>
2026-03-24 16:19:50 +0100

    ajout de l'exo 3

---
+++ hello.py
@@ -0,0 +1,10 @@
+##print("bonjour", end=" " sep="-")
+print("alice")
+age=20
+age2=12
+a_permis=True
+peut_conduire=(age >= 18 and a_permis)
+print("waiy", "tu", "peux", end=" ")
+print(peut_conduire)
+peut_conduire=age2 >= 18 and a_permis
+print(peut_conduire)
---
+++ meteo.py
@@ -0,0 +1,18 @@
+"""nbre=float(input("donnez votre note "))
+if nbre==1234:
+      print("acces autorisé")
+else:
+      print("veuillez recommencer")"""
+
+note=float(input("donnez votre note"))
+if note>=16:
+      print("mention tres bien")
+elif note>=14:
+      print ("mention bien")
+elif note>=12:
+      print("mention  assez bien")
+elif note>=10:
+      print("mention passable")
+else:  
+      print("insuffisant")                    
+wsl --install
