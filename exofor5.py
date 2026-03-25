31597117c8fa00aa014842fac123cf6d22214891
Darryl Fonkou <darrylfonkou@gmail.com>
2026-03-25 12:05:30 +0100

    yes

---
+++ exofor5.py
@@ -0,0 +1,9 @@
+N=int(input("entrez un entier"))
+
+for i in range (N):
+      if i%2==0:
+            print(i, end=" ")
+print("       ")
+for i in range (12):
+      print(i,"x",N,"=",i*N)            
+
