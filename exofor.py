f9719968ef11b004ef35709943a2ef66ddd80874
Darryl Fonkou <darrylfonkou@gmail.com>
2026-03-25 10:33:17 +0100

    exofor_si

--- exofor.py
+++ exofor.py
@@ -1,3 +1,11 @@
-tampon=int(input("donnez moi un nombre "))
-for i in range (1,11):
-      print(f"{i}","x",tampon,"=",i*tampon, sep=" ")
+tampon=int(input("donnez moi un nombre N "))
+print("vous voulez sa table de mutiplication((mu) ou la somme des nbres de 1 à N(som)")
+N=input("")
+q=tampon+1
+if N=="mu":
+      for i in range (1,11):
+            print(f"{i}","x",tampon,"=",i*tampon, sep=" ")
+else:
+      for i in range (1,tampon):
+            tampon=tampon+i
+      print(f"{tampon}")      
